2bf99e72a8743596a5f35241b465e827c8984c0b
Nader Sipahy <nadersip@Naders-MacBook-Pro.local>
2025-09-29 18:07:10 -0400

    adding nmap scan

--- nmap-scan.py
+++ nmap-scan.py
@@ -4,3 +4,4 @@ def network_scan():
     network = input("Veuillez saisir le reseau pour scanner avec mask ex: 192.168.10.0/24 : ")
     scan = os.system("nmap -sn " + network)
     print(scan)
+network_scan()
