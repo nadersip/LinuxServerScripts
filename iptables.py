85e4cb164eb12d58725e205843f6a36a66a6ffc6
Nader Sipahy <nadersip@Naders-MacBook-Pro.local>
2025-11-06 18:00:51 -0500

    adding iptables

--- iptables.py
+++ iptables.py
@@ -1,12 +1,13 @@
 import os
 
 def block_input_fr():
+    protocol = input("Please enter the proctocol : ")
     source = input("Please enter the IP or Network address of the manchine or network you would like to block: ")
     destinatination=  input("Please enter the IP or network of the destination: ")
     source_port= input("Please enter the source port: ")
-    destinatination_port = input("please enter the destination port ")
+    destinatination_port = input("please enter the destination port: ")
     action = input ("please enter the action (ACCEPT, DROP, REJECT): ")
-    rule = os.system("iptables -A INPUT " + "-s " + source + "--sport " + source_port + "-d " + destinatination + "--dport " + destinatination_port + "-j " + action)
+    rule = os.system("iptables -A INPUT " + "-p " + protocol + "-s " + source + "--sport " + source_port + "-d " + destinatination + "--dport " + destinatination_port + "-j " + action)
     print(rule)
 
 block_input_fr()
